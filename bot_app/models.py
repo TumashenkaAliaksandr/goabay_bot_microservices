2bb739250cafab063a6c250b44ba2fb844dd9731
TumashenkaAliaksandr <Badminton500@inbox.lv>
2024-09-24 00:00:31 +0300

    added nem model Product

--- bot_app/models.py
+++ bot_app/models.py
@@ -25,3 +25,17 @@ class UserRegistration(models.Model):
 
     def __str__(self):
         return f"Registration for user {self.user_id}"
+
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255, blank=True, null=True)
+    name = models.TextField(max_length=255, blank=True, null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Price', default=0.00)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Services"
+        verbose_name_plural = "Services"
