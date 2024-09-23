5e6075dac997a73a8c05388753524a6261676723
TumashenkaAliaksandr <Badminton500@inbox.lv>
2024-09-24 00:05:31 +0300

    added new field for the Product model - image

--- bot_app/models.py
+++ bot_app/models.py
@@ -27,15 +27,15 @@ class UserRegistration(models.Model):
         return f"Registration for user {self.user_id}"
 
 
-
 class Product(models.Model):
     name = models.CharField(max_length=255, blank=True, null=True)
-    name = models.TextField(max_length=255, blank=True, null=True)
+    desc = models.TextField(max_length=255, blank=True, null=True)
     price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Price', default=0.00)
+    image = models.ImageField(upload_to='products', verbose_name='photo')
 
     def __str__(self):
         return self.name
 
     class Meta:
-        verbose_name = "Services"
-        verbose_name_plural = "Services"
+        verbose_name = "Products"
+        verbose_name_plural = "Products"
