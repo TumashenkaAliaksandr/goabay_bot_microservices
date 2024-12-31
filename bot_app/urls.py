753f4e735e656d7941a7ba8d9289803ee77a76af
TumashenkaAliaksandr <Badminton500@inbox.lv>
2024-12-31 10:12:55 +0300

    fixing css, fixing import views index in the bot app urls

--- bot_app/urls.py
+++ bot_app/urls.py
@@ -2,9 +2,10 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
-from . import views
+
 from django.views.decorators.csrf import csrf_exempt
 
+from site_app import views
 
 urlpatterns = [
     path('', csrf_exempt(views.index), name='index'),
